a1fcd994062eb06aef19c81258580da63aa2af83
FAIDINE <mohamed.faidine@live.fr>
2021-11-29 09:47:51 +0100

    feat(footer) add footer

---
+++ src/Components/UI/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Fb from '../../../Assets/images/footer/fb.png';
+import Twitter from '../../../Assets/images/footer/twitter.png';
+import Youtube from '../../../Assets/images/footer/youtube.png';
+
+const footer = (props) => (
+    <footer className="bg-secondary container-fluid">
+        <div className="text-white text-center">
+            My Zoo - 2021
+            <div className="row no-gutters align-items-center text-center pt-2">
+                <div className="col-4">
+                    <a href="https://www.facebook.com" className="d-block" target="_blank" rel="noreferrer">
+                        <img src={Fb} alt="facebook" className="wImgFooter" />
+                    </a>
+                </div>
+                <div className="col-4">
+                    <a href="https://www.twitter.com" className="d-block" target="_blank" rel="noreferrer">
+                        <img src={Twitter} alt="twitter" className="wImgFooter" />
+                    </a>
+                </div>
+                <div className="col-4">
+                    <a href="https://www.youtube.com" className="d-block" target="_blank" rel="noreferrer">
+                        <img src={Youtube} alt="youtube" className="youtFooter" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+);
+
+export default footer;
--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -3,18 +3,23 @@ import Navbar from '../../Components/UI/Navbar/Navbar';
 import { Switch, Route} from 'react-router-dom';
 import Home from './Home/Home';
 import Error404 from '../../Components/UI/Error/Error404/Error404';
+import Footer from '../../Components/UI/Footer/Footer';
 class Main extends Component {
     render() {
         return (
             <>
-                <Navbar/>
-                <main>
-                    <Switch>
-                        <Route path="/" exact render={() => <Home/> }/>
-                        <Route path="/contact" exact render={() => <h1>Contact</h1>}/>
-                        <Route path="*" exact component={Error404}/>
-                    </Switch>
-                </main>
+                <div className="monSite">
+                    <Navbar/>
+                    <main>
+                        <Switch>
+                            <Route path="/" exact render={() => <Home/> }/>
+                            <Route path="/contact" exact render={() => <h1>Contact</h1>}/>
+                            <Route path="*" exact component={Error404}/>
+                        </Switch>
+                    </main>
+                    <div className="minSite"></div>
+                </div>
+                <Footer/>
             </>
         )
     };
