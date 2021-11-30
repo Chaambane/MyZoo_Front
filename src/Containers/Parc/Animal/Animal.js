5ef7a838b713b51c0df2b7d4cf78aba8418ec6e3
FAIDINE <mohamed.faidine@live.fr>
2021-11-30 13:27:08 +0100

    feat(parc) add display animal in the component animal

---
+++ src/Containers/Parc/Animal/Animal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from '../../../Components/UI/Button/Button';
+
+const animal = (props) => (
+    <div className="card">
+        <h2 className="card-header text-center fw-bold">
+            {props.id} - {props.nom}
+        </h2>
+        <div className="card-body text-center">
+            <div className="card-text">{props.description}</div>
+            <div>
+                <img src={props.image} className="img-fluid h-100" width="80px" alt={props.nom} />
+            </div>
+            <h3 className="card-text p-2">Famille: <Button btnClr="btn-secondary">{props.famille.libelleFamille.toUpperCase()}</Button></h3>
+            <p className="card-text p-2">{props.famille.descriptionFamille}</p>
+            {props.continents.map(continent => {
+                let colorBtn = "";
+                switch(continent.idContinent) {
+                    case "1": colorBtn = "btn-secondary";
+                    break;
+                    case "2": colorBtn = "btn-info";
+                    break;
+                    case "3": colorBtn = "btn-primary";
+                    break;
+                    case "4": colorBtn = "btn-warning";
+                    break;
+                    case "5": colorBtn = "btn-danger";
+                    break;
+                    default: return null;
+                }
+                return(
+                    <Button btnClr={colorBtn} cssM="me-1" key={continent.idContinent}>{continent.libelleContinent}</Button>
+                )
+            })}
+        </div>
+    </div>
+);
+
+export default animal;
--- src/Containers/Parc/Parc.js
+++ src/Containers/Parc/Parc.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
 import Title from '../../Components/UI/Title/Title';
+import axios from 'axios';
+import Animal from './Animal/Animal';
+
 
 class Parc extends Component {
+    state= {
+        DataParc: null
+    }
+
+    componentDidMount = () => {
+        axios.get('http://localhost:8090/SERVEUR_ANIMAUX/front/animaux')
+            .then(response => {
+                console.log(response.data);
+                this.setState({DataParc: Object.values(response.data)})
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     render() {
         return(
             <>
                 <Title bgClr="bg-success">Animaux dans le parc</Title>
                 <div className="container">
-                    Les animaux du parc
+                    {
+                        this.state.DataParc &&
+                        <div className="row no-gutters">
+                            {
+                                this.state.DataParc.map(animal => {
+                                    return(
+                                        <div className="col-12 col-md-6 mt-2 mb-2" key={animal.id}>
+                                            <Animal {...animal}/>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    }
                 </div>
             </>
         )
