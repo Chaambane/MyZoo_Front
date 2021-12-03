4b1218a6ef287952a66fc2f4637661d9a2852b2b
FAIDINE <mohamed.faidine@live.fr>
2021-12-03 10:50:06 +0100

    feat(Parc) add name in the button filtered

--- src/Containers/Parc/Parc.js
+++ src/Containers/Parc/Parc.js
@@ -78,8 +78,23 @@ class Parc extends Component {
         this.setState({filterContinent: null});
     }
 
-
     render() {
+        let filteredFamilyName = "";
+        if(this.state.filterFamille) {
+            const filteredFamilyBoxNumber = this.state.listeFamilles.findIndex(famille => {
+                return famille.famille_id === this.state.filterFamille;
+            })
+            filteredFamilyName = this.state.listeFamilles[filteredFamilyBoxNumber].famille_libelle;
+        }
+
+        let filteredContinentName = "";
+        if(this.state.filterContinent) {
+            const filteredContinentBoxNumber = this.state.listeContinents.findIndex(continent => {
+                return continent.continent_id === this.state.filterContinent;
+            })
+            filteredContinentName = this.state.listeContinents[filteredContinentBoxNumber].continent_libelle;
+        }
+
         return(
             <>
                 <Title bgClr="bg-success">Animaux dans le parc</Title>
@@ -122,7 +137,7 @@ class Parc extends Component {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                         </svg> &nbsp;
-                        {this.state.filterFamille}
+                        {filteredFamilyName}
                         </Button>
                     }
                     {
@@ -134,7 +149,7 @@ class Parc extends Component {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                         </svg> &nbsp;
-                        {this.state.filterContinent}</Button>
+                        {filteredContinentName}</Button>
                     }
                     {
                         this.state.DataParc &&
